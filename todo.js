307e3b5f00dcbcf90e00640fc5d4d0170602d504
dhananjay431 <dbel431@gmail.com>
2017-11-09 15:24:10 +0530

    init

---
+++ todo.js
@@ -0,0 +1,68 @@
+
+angular.module('todoApp.login', ['ui.router'])
+.config(function($stateProvider,$urlRouterProvider,$locationProvider) {
+  // $locationProvider.html5Mode(true);
+  // $urlRouterProvider.otherwise('/login');
+  $stateProvider.state('login', {
+    url: '/login',
+	templateUrl: 'login.html',
+  	controller:'todoApp.login.ctrl'
+    });
+
+}).controller('todoApp.login.ctrl', function() {
+    var todoList = this;
+    console.log('todoApp.login');
+});
+
+
+angular.module('todoApp.menu', ['ui.router','ckeditor'])
+.config(function($stateProvider,$urlRouterProvider,$locationProvider) {
+// $urlRouterProvider.otherwise('/menu');
+//   $locationProvider.html5Mode(true);
+  $stateProvider.state('menu', {
+    url: '/menu',
+    //template: '<h1>dfdfsdf</h1>',
+	templateUrl: 'menu.html',
+ 	controller:'todoApp.menu.ctrl'
+    })
+  	.state('menu.sub', {
+    url: '/sub',
+	templateUrl: 'sub.html',
+ 	controller:'sub.menu.ctrl'
+    });
+
+}).controller('todoApp.menu.ctrl', function() {
+    var todoList = this;
+     todoList.options = {
+        language: 'en',
+        allowedContent: true,
+        entities: false
+      };
+
+  // Called when the editor is completely ready.
+    todoList.onReady = function () {
+      console.log("onReady");
+    };
+    console.log('todoApp.menu.ctrl');
+}).controller('sub.menu.ctrl', function() {
+    var todoList = this;
+    console.log('sub.menu.ctrl');
+});
+
+
+angular.module('todoApp', [
+	'todoApp.login',
+	'todoApp.menu',
+	'ui.router']).controller('TodoListController', function() {
+    var todoList = this;
+    console.log('TodoListController');
+}).config(function($stateProvider,$urlRouterProvider,$locationProvider) {
+$urlRouterProvider.otherwise('/login');
+  $locationProvider.html5Mode(true);
+ //  $stateProvider.state('menu', {
+ //    url: '/menu',
+	// templateUrl: 'menu.html',
+ // 	controller:'todoApp.menu.ctrl'
+ //    });
+
+})
