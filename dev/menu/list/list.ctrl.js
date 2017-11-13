407b2b746b55132208e229274d84908b172ad57c
dhananjay431 <dbel431@gmail.com>
2017-11-13 17:52:35 +0530

    add datatable

--- dev/menu/list/list.ctrl.js
+++ dev/menu/list/list.ctrl.js
@@ -2,7 +2,7 @@ angular.module('todoApp.menu')
 .controller('list.menu.ctrl', function($resource,DTOptionsBuilder, DTColumnBuilder) {
     var todoList = this;
     console.log('sub.menu.ctrl');
-    var x= $resource('https://jsonplaceholder.typicode.com/users',{},{get:{method:"GET",isArray:true}});
+    var x= $resource('https://jsonplaceholder.typicode.com/comments',{},{get:{method:"GET",isArray:true}});
    todoList.dtOptions = DTOptionsBuilder.fromSource(x.get)
         .withPaginationType('full_numbers');
    todoList.dtColumns = [
