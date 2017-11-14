6f244afab938c1457bd2fbc5cab385f8b387d53c
dhananjay431 <dbel431@gmail.com>
2017-11-14 18:10:15 +0530

    change url

--- public/js/controller.js
+++ public/js/controller.js
@@ -39,11 +39,14 @@ angular.module('todoApp.menu')
     console.log('sub.menu.ctrl');
     var x= $resource('https://jsonplaceholder.typicode.com/comments',{},{get:{method:"GET",isArray:true}});
    todoList.dtOptions = DTOptionsBuilder.fromSource(x.get)
+        //  .withOption('serverSide', true)
+        //   .withOption('processing', true)
         .withPaginationType('full_numbers');
    todoList.dtColumns = [
         DTColumnBuilder.newColumn('id').withTitle('ID'),
         DTColumnBuilder.newColumn('name').withTitle('First name'),
-        DTColumnBuilder.newColumn('email').withTitle('email')
+        DTColumnBuilder.newColumn('email').withTitle('email'),
+        DTColumnBuilder.newColumn('body').withTitle('Description')
     ];
 // x.get({}).$promise.then(function(data){
 //   console.log(data);
